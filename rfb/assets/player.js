6336ad939244dea78d3038a757a2a18457dfea9b
Thijs van Dijk <github@inurbanus.nl>
2020-07-28 22:42:17 +0200

    Add a 'play' button

--- rfb/assets/player.js
+++ rfb/assets/player.js
@@ -4,6 +4,9 @@ class RFB {
 		this.width = width;
 		this.height = height;
 		this.tmax = 0.0;
+		this.currentTime = 0.0;
+		this.playing = false;
+		this.lastFrame = 0.0;
 		this.events = [];
 	}
 
@@ -18,6 +21,8 @@ class RFB {
 	}
 
 	Render(elt) {
+		this.tmax = Math.floor( this.tmax + 250 );
+
 		elt.innerHTML = "";
 
 		this.canvas = document.createElement("canvas");
@@ -31,11 +36,17 @@ class RFB {
 
 		let controls = document.createElement("div");
 
+		this.playbutton = document.createElement("button");
+		this.playbutton.innerText = "";
+		this.playbutton.addEventListener("click", () => this.TogglePlaying());
+		this.playbutton.style.width = "4rem";
+		controls.appendChild(this.playbutton);
+
 		this.seekbar = document.createElement("input");
 		this.seekbar.id = "seekbar_" + Math.random().toString(36).substr(2);
 		this.seekbar.type = "range";
 		this.seekbar.min = 0;
-		this.seekbar.max = Math.floor( this.tmax + 250 );
+		this.seekbar.max = this.tmax;
 		this.seekbar.step = 0.1;
 		this.seekbar.value = 0;
 
@@ -62,11 +73,50 @@ class RFB {
 		this.ctx.fillRect( 0, 0, this.width, this.height );
 	}
 
+	Play() {
+		this.playing = true;
+		this.playbutton.innerText = "\u258c\u258c";
+
+		window.requestAnimationFrame( (time) => {
+			this.lastFrame = time;
+			window.requestAnimationFrame( (t) => this.nextFrame(t) );
+		} );
+	}
+
+	Pause() {
+		this.playing = false;
+		this.playbutton.innerText = "\u25b6";
+	}
+
+	TogglePlaying() {
+		if ( this.playing ) {
+			this.Pause();
+		} else {
+			this.Play();
+		}
+	}
+
+	nextFrame( time ) {
+		if ( !this.playing ) {
+			return;
+		}
+		let tnew = this.currentTime + ( time - this.lastFrame );
+		if ( tnew > this.tmax ) {
+			tnew = this.tmax;
+		}
+		this.setTime(tnew);
+
+		this.lastFrame = time;
+		window.requestAnimationFrame( (t) => this.nextFrame(t) );
+	}
+
 	seek() {
 		this.setTime(this.seekbar.value);
 	}
 
 	setTime( time ) {
+		this.currentTime = time;
+
 		let i, n = 0;
 		for ( i = 0; i < this.events.length; i++ ) {
 			if ( this.events[i].time < time ) {
